597c67a0990ba19e80a27ea82bf22f9a6da8f89d
Trinity <102254341+twoichai@users.noreply.github.com>
2023-03-07 20:50:11 -0800

    test

--- .idea/src/ToDoTask.java
+++ .idea/src/ToDoTask.java
@@ -1,8 +1,20 @@
+/**
+ * This Class will include the main instances, constructors and methods
+ */
+
 import java.util.Date;
 
 public class ToDoTask {
+    // instances
     String taskName;
     String taskText;
     Date createdAt;
     Date deadline;
+
+
+    // constructors
+    public ToDoTask(){
+
+    }
+    // methods
 }
