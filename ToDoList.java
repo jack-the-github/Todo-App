0ca873ca638c87fda5d0b855d292733697f292de
Trinity <102254341+twoichai@users.noreply.github.com>
2023-03-07 21:18:55 -0800

    Version with decription and default constructor

--- ToDoList.java
+++ ToDoList.java
@@ -6,15 +6,31 @@ import java.util.Date;
 
 public class ToDoTask {
     // instances
-    String taskName;
-    String taskText;
-    Date createdAt;
-    Date deadline;
+    private String taskName;
+    private String taskText;
+    private Date createdAt;
+    private Date deadline;
 
 
     // constructors
-    public ToDoTask(){
+    //default constructor
 
+    /**
+     * Default Constructor
+     */
+    public ToDoTask(){
     }
+    /**
+     * Constructor with task name and task description
+     * @param taskName as name of the task
+     * @param taskText as description of the task
+     */
+    public ToDoTask(String taskName, String taskText){
+        this.taskName = taskName;
+        this.taskText = taskText;
+    }
+
+
+
     // methods
 }
