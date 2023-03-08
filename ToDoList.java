56472efdbeff298aa53a169e3b9577bac6cd541f
Trinity <102254341+twoichai@users.noreply.github.com>
2023-03-07 21:08:00 -0800

    The version with description

--- ToDoList.java
+++ ToDoList.java
@@ -1,2 +1,20 @@
-public class ToDoList {
+/**
+ * This Class will include the main instances, constructors and methods
+ */
+
+import java.util.Date;
+
+public class ToDoTask {
+    // instances
+    String taskName;
+    String taskText;
+    Date createdAt;
+    Date deadline;
+
+
+    // constructors
+    public ToDoTask(){
+
+    }
+    // methods
 }
